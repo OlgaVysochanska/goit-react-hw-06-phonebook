2f22391c9df2d54c3655cd150be640bdcc2cc2b0
OlgaVysochanska <olavysochanska@gmail.com>
2023-02-24 11:52:39 +0200

    add middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { persistStore } from 'redux-persist';
 
 import { persistedReducer } from './root-reducer';
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: false,
+  }),
 });
 
 export const persistor = persistStore(store);
